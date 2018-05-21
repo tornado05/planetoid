1c8c228a0a75abbd9b5de8655b314c02e89b4c58
Andrew Serheichuk <sergeichuk.andrew@gmail.com>
2018-05-22 00:42:48 +0300

    adds zoom plugin

--- src/plugins/zoom.js
+++ src/plugins/zoom.js
@@ -1,7 +1,25 @@
 import PlanetoidPlugin from "./plugin"
+import { zoom } from "d3-zoom"
+import { select, event } from "d3-selection"
+
+const DEFAULT_INITIAL_SCALE = 1
+const DEFAULT_SCALE_EXTENT = [200, 2000]
 
 export default class ZoomPlugin extends PlanetoidPlugin{
     constructor(options={}) {
-        super()        
+        super()
+        this.initialScale = options.initialScale ? options.initialScale : DEFAULT_INITIAL_SCALE        
+        this.scaleExtent = options.scaleExtent ? options.scaleExtent : DEFAULT_SCALE_EXTENT
+    }
+
+    initialize(canvas, projection) {
+        this.projection = projection
+        this.zoom = zoom().scaleExtent(this.scaleExtent)
+            .on("zoom", this.onZoom.bind(this))
+        select(canvas).call(this.zoom)
+    }
+
+    onZoom () {
+        this.projection.scale(event.transform.k)
     }
 }
