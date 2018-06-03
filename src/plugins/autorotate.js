23e0da9498ff4d4f6bf5ed6a7f8267031d7be17b
tornado05 <sergeichuk.andrew@gmail.com>
2018-06-03 22:33:54 +0300

    makes autorotate plugin blockable by drag plugin

--- src/plugins/autorotate.js
+++ src/plugins/autorotate.js
@@ -2,11 +2,30 @@ import PlanetoidPlugin from "./plugin"
 
 const rotate = [10, -10]
 const velocity = [.003, -.001]
-const time = Date.now()
+const startTime = Date.now()
+
+const BLOCKING_EVENTS = {
+    DRAGSTART: "dragstart",
+    DRAGEND: "dragend"
+}
 
 export default class AutoRotatePlugin extends PlanetoidPlugin{
+
+    constructor() {
+        super()
+        this.enabled = true
+    }
+
+    initialize({canvas, projection}) {
+        this.addEventListener(BLOCKING_EVENTS.DRAGSTART, () => this.enabled = false)
+        this.addEventListener(BLOCKING_EVENTS.DRAGEND, () => this.enabled = true)
+        return super.initialize({canvas, projection})
+    }
+
     draw({projection}) {
-        let dt = Date.now() - time
-        projection.rotate([rotate[0] + velocity[0] * dt, rotate[1] + velocity[1] * dt])
+        if (this.enabled) {
+            let timeDelta = Date.now() - startTime
+            projection.rotate([rotate[0] + velocity[0] * timeDelta, rotate[1] + velocity[1] * timeDelta])
+        }
     }
 }
--- src/plugins/drag.js
+++ src/plugins/drag.js
@@ -3,6 +3,17 @@ import { event, select } from "d3-selection"
 import { scaleLinear } from "d3-scale"
 import { drag } from "d3-drag"
 
+const PLUGIN_EVENTS = {
+    DRAGSTART: "dragstart",
+    DRAGEND: "dragend"
+}
+
+const DRAG_EVENTS = {
+    START: "start",
+    END: "end",
+    DRAG: "drag"
+}
+
 export default class DragPlugin extends PlanetoidPlugin{
     constructor(options={}) {
         super()        
@@ -11,34 +22,29 @@ export default class DragPlugin extends PlanetoidPlugin{
     initialize ({canvas, projection}) {
         this.projection = projection
         this.drag = drag()
-            .on("start", this.onDragStart.bind(this))
-            .on("end", this.onDragEnd.bind(this))
-            .on("drag", this.onDrag.bind(this))
+            .on(DRAG_EVENTS.START, this.onDragStart.bind(this))
+            .on(DRAG_EVENTS.END, this.onDragEnd.bind(this))
+            .on(DRAG_EVENTS.DRAG, this.onDrag.bind(this))
         select(canvas).call(this.drag)
     }
 
     onDragStart () {
+        this.notify({name: PLUGIN_EVENTS.DRAGSTART})
     }
 
     onDragEnd () {
-
+        this.notify({name: PLUGIN_EVENTS.DRAGEND})
     }
 
     onDrag () {
-        const dx = event.dx
-        const dy = event.dy
         let rotation = this.projection.rotate()
         const radius = this.projection.scale()
         const scaler = scaleLinear().domain([-1 * radius, radius]).range([-90, 90])
-        rotation[0] += scaler(dx)
-        rotation[1] -= scaler(dy)
+        rotation[0] += scaler(event.dx)
+        rotation[1] -= scaler(event.dy)
         if (rotation[1] > 90)   rotation[1] = 90
         if (rotation[1] < -90)  rotation[1] = -90
         if (rotation[0] >= 180) rotation[0] -= 360
         this.projection.rotate(rotation)
-        this.afterDrag()
-    }
-
-    afterDrag () {
     }
 }
